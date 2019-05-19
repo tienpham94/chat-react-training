62fb297b9c4e6329780d855dcd81cdd2eef6b8d7
Tien Pham <tiendev94@gmail.com>
2019-05-19 15:49:23 +0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { useState, useEffect } from "react";
-import firebase from "firebase";
-import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDy-UgSp4weo2sTzJxTiIuWeYOklCzWMcI",
-  authDomain: "chat-app-75ae5.firebaseapp.com",
-  databaseURL: "https://chat-app-75ae5.firebaseio.com",
-  projectId: "chat-app-75ae5",
-  storageBucket: "chat-app-75ae5.appspot.com",
-  messagingSenderId: "404332746183",
-  appId: "1:404332746183:web:e066ad5c5efb1d10"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
+import { db } from "./firebase";
 
 function App() {
   const [channels, setChannels] = useState([]);
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy-UgSp4weo2sTzJxTiIuWeYOklCzWMcI",
+  authDomain: "chat-app-75ae5.firebaseapp.com",
+  databaseURL: "https://chat-app-75ae5.firebaseio.com",
+  projectId: "chat-app-75ae5",
+  storageBucket: "chat-app-75ae5.appspot.com",
+  messagingSenderId: "404332746183",
+  appId: "1:404332746183:web:e066ad5c5efb1d10"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
