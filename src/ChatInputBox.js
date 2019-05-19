b69996d5a55ebaf908fca94f439d2f358b77e3c3
Tien Pham <tiendev94@gmail.com>
2019-05-19 16:17:45 +0300

    value

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function ChatInputBox() {
   return (
-    <div className="ChatInputBox">
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        const value = e.target.elements[0].value;
+        console.log(value);
+      }}
+      className="ChatInputBox"
+    >
       <input className="ChatInput" placeholder="Message #general" />
-    </div>
+    </form>
   );
 }
 
